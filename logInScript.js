af3b3343fcacf72288bdc548b5b622e89a42bba5
nasirmasud <nasir.masud@ymail.com>
2026-03-08 15:24:29 +0600

    Added Login Function

--- logInScript.js
+++ logInScript.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const logInBtn = document.getElementById("login-btn");
+
+if (logInBtn) {
+  logInBtn.addEventListener("click", function () {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    if (user === "admin" && pass === "admin123") {
+      window.location.href = "main.html";
+    } else {
+      alert("Invalid username or password. Please try again.");
+    }
+  });
+}
