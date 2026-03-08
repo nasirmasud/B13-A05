a97e0de5fa264f810f4e95aea6d02bb534988c8f
nasirmasud <nasir.masud@ymail.com>
2026-03-08 20:11:23 +0600

    Display all card content

--- mainScript.js
+++ mainScript.js
@@ -1,1 +1,55 @@
-console.log("hello");
+let allIssues = [];
+const issueContainer = document.getElementById("issue-container");
+
+async function loadAllIssues() {
+  const res = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  const data = await res.json();
+  allIssues = data.data;
+
+  displayIssues(allIssues);
+}
+
+function displayIssues(issues) {
+  issues.forEach((issue) => {
+    const isOpen = issue.status === "open";
+
+    const card = document.createElement("div");
+    card.className = `card bg-white border-t-4 ${isOpen ? "border-t-emerald-500" : "border-t-indigo-500"} shadow-sm rounded-xl overflow-hidden hover:shadow-md transition-shadow cursor-pointer`;
+
+    card.innerHTML = `
+      <div class="p-5">
+        <div class="flex justify-between items-start mb-3">
+          <span class="bg-emerald-50 p-1.5 rounded-full text-xs">
+            <img src='./assets/Open-Status.png' alt='status' />
+          </span>
+          <span class="badge bg-red-200 text-[10px] font-bold py-1 border-none">
+            ${issue.priority.toUpperCase()}
+          </span>
+        </div>
+        <h3 class="font-bold text-gray-800 mb-2 leading-snug">${issue.title}</h3>
+        <p class="text-xs text-gray-500 line-clamp-2 mb-4">${issue.description}</p>
+        <div class="flex flex-wrap gap-1 mb-6">
+                      <div class="flex gap-2 mb-6">
+              <span
+                class="badge badge-sm bg-red-50 text-red-500 border-none text-[10px] font-bold"
+                >BUG</span
+              >
+              <span
+                class="badge badge-sm bg-orange-50 text-orange-500 border-none text-[10px] font-bold"
+                >HELP WANTED</span
+              >
+            </div>
+        </div>
+        <div class="border-t pt-4 text-[11px] text-gray-400">
+          <p>#${issue.id} by ${issue.author}</p>
+          <p>${new Date(issue.createdAt).toLocaleDateString("en-US")}</p>
+        </div>
+      </div>`;
+
+    issueContainer.appendChild(card);
+  });
+}
+
+loadAllIssues();
